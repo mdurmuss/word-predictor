e122d66bc7656ac44ed5ace68cc7db82d81adfee
mustafa <mustafa-durmuss@outlook.com>
2020-12-12 22:46:33 +0300

    daily updates for app.py :zap:

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template, redirect, url_for, request
 import sys
 sys.path.insert(1, '/home/hummingbird/Desktop/MD/github/word-predictor')
 from predictor import WordPredictor
-
+from spell_check import spell_checking
 
 app = Flask(__name__)
 
@@ -16,18 +16,41 @@ predictor = WordPredictor('./corpora/little_prince.txt')
 
 result = []
 word = ""
+is_in_dict_flag = True
+did_u_mean_words = []
+
 @app.route("/")
 def index():
-    return render_template("index.html", todos=result, input_word=word)
+    return render_template("index.html", todos=result, input_word=word,
+                           is_in_dict_flag=is_in_dict_flag,
+                           did_u_mean_words=did_u_mean_words)
 
 
 @app.route("/add",methods = ["POST"])
 def addTodo():
-    global result, word
+    global result, word, is_in_dict_flag, did_u_mean_words
     # kelime
     word = request.form.get("title")
+    if not word == "":
+        # sonraki kelimeler ve ihtimalleri bir dizi şeklinde elde edilir.
+        result = predictor.predict(word, return_flag=True)
+        if not result:
+            is_in_dict_flag = False
+            did_u_mean_words = spell_checking(word, predictor)
+        else:
+            is_in_dict_flag = True
+    else:
+        result = []
+    return redirect(url_for("index"))
+
+
+@app.route("/test_add", methods = ["POST"])
+def test_button():
+    global result, word, is_in_dict_flag
+    word = request.form['test_button']
     # sonraki kelimeler ve ihtimalleri bir dizi şeklinde elde edilir.
     result = predictor.predict(word, return_flag=True)
+    is_in_dict_flag = True
     return redirect(url_for("index"))
 
 
